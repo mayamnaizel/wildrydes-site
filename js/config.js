6126cbd7ec2bf119b057c3bb6dee98e068da851e
Maya Mnaizel <105586284+mayamnaizel@users.noreply.github.com>
2025-03-01 17:50:54 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_11DrvOPEl', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3fmi0j033if0dp0p1gegvesl8n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'fpfk8r72amjg66nj1ltjujhku', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
